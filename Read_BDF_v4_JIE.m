59b22855910741a7d583c94d65051a308d59940b
jiegana <101658966+jiegana@users.noreply.github.com>
2022-03-16 12:24:21 -0300

    Update Read_BDF_v4_JIE.m

--- Read_BDF_v4_JIE.m
+++ Read_BDF_v4_JIE.m
@@ -13,17 +13,25 @@
 %                   
 % Plastikfaith November 2021
 
+%%
+clear all                       %remove if necessary
+close all                       %remove if necessary
 
-clear all
+%% Select path to data (.bdf) files
+datapath = '/Users/jiegana/Google Drive/Lab_GooDrive/Proyecto_EEG_Anesthesia_Pain/Proyecto_BNI/Pain_for_Purdon_Lab/PainData_fPL';
 
 
-fileData = dir('*.bdf');            %Encuentra los archivos .bdf dentro del
-fileNames = {fileData.name};        %directorio especificado y saca nombres
-narchi = length(fileNames);
+%% Find folders with EEG files inside
+cd (datapath);
+filelist = dir(fullfile(datapath, '**/*.bdf'));
+for tempidx = 1:length(filelist)
+    numsiname = regexp(filelist(tempidx).folder,'\d+','Match');
+    filelist(tempidx).pacnum = str2double(numsiname{1});
+end
 
 %%
-for fileindex = 1:narchi
-    archi = fileNames{fileindex};
+for fileindex = 1%:tempidx
+    archi = [filelist(tempidx).folder '/' filelist(tempidx).name];
     [sig, head] = sload (archi,'BDF[4]');
     Trig(:,1) = head.BDF.Trigger.TYP;   % TTL identifier
     Trig(:,2) = head.BDF.Trigger.POS;   % Time position in sample number
