c52740556f949da75b6e4ad302fb8842f6b86ecd
jiegana <101658966+jiegana@users.noreply.github.com>
2022-03-21 15:52:57 -0300

    Update Read_BDF_v4_JIE.m

--- Read_BDF/Read_BDF_v4_JIE.m
+++ Read_BDF/Read_BDF_v4_JIE.m
@@ -7,23 +7,27 @@
 %
 % Trig contains TTL information
 % sig contains signal
-% head contains file's header 
+% head contains file's header
 %
 % Use at your own risk
-%                   
+%
 % Plastikfaith November 2021
 
 %%
 clear all                       %remove if necessary
 close all                       %remove if necessary
-
+%% Check Biosig sload function is available
+A = exist('sload'); %#ok<EXIST>
+if A == 0
+    cd ('/Users/jiegana/Documents/Scripts_MatLab/biosig4octmat-2.92')
+    biosig_installer
+end
 %% Select path to data (.bdf) files
 datapath = '/Users/jiegana/Google Drive/Lab_GooDrive/Proyecto_EEG_Anesthesia_Pain/Proyecto_BNI/Pain_for_Purdon_Lab/PainData_fPL';
-
-
 %% Find files and folders with EEG(bdf) files inside datapath
 cd (datapath);
 filelist = dir(fullfile(datapath, '**/*.bdf'));
+%%
 for tempidx = 1:length(filelist)
     numsiname = regexp(filelist(tempidx).folder,'\d+','Match'); %finds subject's ID number
     filelist(tempidx).pacnum = str2double(numsiname{1});        %add field to structure subject's number
@@ -33,10 +37,10 @@ end
 for fileindex = 1%:tempidx
     archi = [filelist(tempidx).folder '/' filelist(tempidx).name];
     [sig, head] = sload (archi,'BDF[4]');
-
+    
     BDFJIE.event(:,1) = head.BDF.Trigger.TYP;   % TTL identifier
     BDFJIE.event(:,2) = head.BDF.Trigger.POS;   % Time position in sample number
-    BDFJIE.event(:,3) = head.SampleRate;        % Sampling rate   
+    BDFJIE.event(:,3) = head.SampleRate;        % Sampling rate
     BDFJIE.signal = sig;
     BDFJIE.header = head;
     
